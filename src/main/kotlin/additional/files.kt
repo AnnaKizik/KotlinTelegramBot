79de735bee3e6e2447f1cd9d33f538ebb1e4f3c8
anna_kizik <a.v.kizik@gmail.com>
2025-06-28 15:00:03 +0300

    KTB-04

--- src/main/kotlin/additional/files.kt
+++ src/main/kotlin/additional/files.kt
@@ -5,5 +5,15 @@ import java.io.File
 fun main() {
     val wordsFile = File("words.txt")
     wordsFile.createNewFile()
-    for (i in wordsFile.readLines()) println(i)
+
+    val lines: List<String> = wordsFile.readLines()
+    val dictionary = mutableListOf<Word>()
+
+    for (line in lines) {
+        val line = line.split("|")
+        val newWord = Word(original = line[0], translate = line[1], correctAnswersCount = line[2].toIntOrNull() ?: 0)
+        dictionary.add(newWord)
+    }
+
+    println(dictionary)
 }
