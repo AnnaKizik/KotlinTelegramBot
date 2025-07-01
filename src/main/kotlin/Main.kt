fb46658ffca63b349c0c653ed0e5ab2553338053
a.kizik <a.v.kizik@gmail.com>
2025-07-01 08:52:08 +0300

    KTB-06

--- src/main/kotlin/Main.kt
+++ src/main/kotlin/Main.kt
@@ -1,6 +1,10 @@
 package org.example
 
+import org.example.additional.loadDictionary
+import java.io.File
+
 fun main() {
+    val dictionary = loadDictionary(File("words.txt"))
 
     while (true) {
         println(
@@ -19,9 +23,16 @@ fun main() {
                 "Выбран раздел \"Учить слова\""
             )
 
-            "2" -> println(
-                "Выбран раздел \"Статистика\""
-            )
+            "2" -> {
+                println(
+                    "Выбран раздел \"Статистика\""
+                )
+                val totalCount = dictionary.size
+                val learnedCount = dictionary.filter { it.correctAnswersCount!! >= 3 }.size
+                val percent = (learnedCount / totalCount) * 100
+                println("Выучено $learnedCount из $totalCount слов | $percent%\n")
+                continue
+            }
 
             "0" -> return
             else -> {
