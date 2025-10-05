34d7f01356847fde3dd47099defdd60de1311ccf
anna_kizik <a.v.kizik@gmail.com>
2025-10-05 22:20:33 +0300

    KTB-23

--- build.gradle.kts
+++ build.gradle.kts
@@ -1,6 +1,9 @@
+
 plugins {
     kotlin("jvm") version "2.2.20"
     kotlin("plugin.serialization") version "2.2.20"
+    id("com.github.johnrengelman.shadow") version "8.1.1"
+    application
 }
 
 group = "org.example"
@@ -18,6 +21,22 @@ dependencies {
 tasks.test {
     useJUnitPlatform()
 }
+
 kotlin {
-    jvmToolchain(22)
+    jvmToolchain(21)
+}
+
+application {
+    mainClass.set("Telegram.kt")
+}
+
+tasks {
+    named<com.github.jengelman.gradle.plugins.shadow.tasks.ShadowJar>("shadowJar") {
+        manifest {
+            attributes(mapOf("Main-Class" to "TelegramKt"))
+        }
+        archiveBaseName.set("bot")
+        archiveClassifier.set("")
+        archiveVersion.set("")
+    }
 }
